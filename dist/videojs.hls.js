6899335a094188a9fc168c378dab455cd18c2000
Mark Ablovacky <mark.ablov@gmail.com>
2016-03-19 13:38:18 +0200

    restore missed pkcs7.unpad

--- dist/videojs.hls.js
+++ dist/videojs.hls.js
@@ -1,4 +1,4 @@
-/*! videojs-contrib-hls - v1.0.0-20 - 2016-03-19
+/*! videojs-contrib-hls - v1.0.0-21 - 2016-03-19
 * Copyright (c) 2016 Brightcove; Licensed  */
 /*! videojs-contrib-media-sources - v2.0.1 - 2016-03-19
 * Copyright (c) 2016 Brightcove; Licensed  */
@@ -5766,6 +5766,35 @@ resolveUrl = videojs.Hls.resolveUrl = function(basePath, path) {
   videojs.Hls.PlaylistLoader = PlaylistLoader;
 })(window, window.videojs);
 
+(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function (global){
+global.window.pkcs7 = {
+  unpad: require('./unpad')
+};
+
+}).call(this,typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
+},{"./unpad":2}],2:[function(require,module,exports){
+/*
+ * pkcs7.unpad
+ * https://github.com/brightcove/pkcs7
+ *
+ * Copyright (c) 2014 Brightcove
+ * Licensed under the apache2 license.
+ */
+
+'use strict';
+
+/**
+ * Returns the subarray of a Uint8Array without PKCS#7 padding.
+ * @param padded {Uint8Array} unencrypted bytes that have been padded
+ * @return {Uint8Array} the unpadded bytes
+ * @see http://tools.ietf.org/html/rfc5652
+ */
+module.exports = function unpad(padded) {
+  return padded.subarray(0, padded.byteLength - padded[padded.byteLength - 1]);
+};
+
+},{}]},{},[1]);
 (function(window, videojs, unpad) {
 'use strict';
 
